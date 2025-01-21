ba64d24abc4b97ab088e2ffc0a33c7abb5adf58f
chdrchz <saavannaa.rose@gmail.com>
2025-01-21 13:11:34 -0600

    song title component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,14 @@
 import Footer from "./Footer";
 import MusicPlayer from "./MusicPlayer";
 import CoverArt from "./CoverArt";
+import SongTitle from "./SongTitle";
 
 function App() {
   return (
     <div className="h-full flex flex-col justify-between p-8 min-h-screen">
       <MusicPlayer />
       <CoverArt />
+      <SongTitle />
       <Footer />
     </div>
   );
---
+++ src/components/SongTitle.jsx
@@ -0,0 +1,5 @@
+export default function SongTitle() {
+    return (
+        <><h1>Tidal Drift</h1><h2>Echoes of the Sea</h2></>
+    )
+}
