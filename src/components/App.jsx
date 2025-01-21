e606ead61c48f3aa8d7e537d7959fbd22810516f
chdrchz <saavannaa.rose@gmail.com>
2025-01-21 12:58:31 -0600

    cover art component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import Footer from "./Footer";
 import MusicPlayer from "./MusicPlayer";
+import CoverArt from "./CoverArt";
 
 function App() {
   return (
     <div className="h-full flex flex-col justify-between p-8 min-h-screen">
       <MusicPlayer />
+      <CoverArt />
       <Footer />
     </div>
   );
---
+++ src/components/CoverArt.jsx
@@ -0,0 +1,7 @@
+import Image from '../assets/placeholder.svg';
+
+export default function CoverArt() {
+    return (
+        <img src={Image} alt="svg" />
+    )
+}
