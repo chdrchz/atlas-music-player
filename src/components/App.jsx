7452178f9e6faf37513059b224480ad0cc954ccc
chdrchz <saavannaa.rose@gmail.com>
2025-01-21 13:28:25 -0600

    cant find speed, but otherwose done with play controls

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import Footer from "./Footer";
 import MusicPlayer from "./MusicPlayer";
 import CoverArt from "./CoverArt";
 import SongTitle from "./SongTitle";
+import PlayControls from "./PlayControls";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <MusicPlayer />
       <CoverArt />
       <SongTitle />
+      <PlayControls />
       <Footer />
     </div>
   );
---
+++ src/components/PlayControls.jsx
@@ -0,0 +1,12 @@
+import { Play, FastForward, Rewind, Shuffle } from "lucide-react";
+
+export default function PlayControls() {
+  return (
+    <>
+      <Rewind />
+      <Play />
+      <FastForward />
+      <Shuffle />
+    </>
+  );
+}
