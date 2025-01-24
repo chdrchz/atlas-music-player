26dbbc666016c430ca3f1b5a3c55c8b1a564a544
chdrchz <saavannaa.rose@gmail.com>
2025-01-24 16:15:08 -0600

    created a current song context

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,15 @@
 import MusicPlayer from "./MusicPlayer.tsx";
 import Footer from "./Footer.tsx";
 
+import { CurrentSongProvider } from './CurrentSongContext';
+
 function App() {
   return (
     <div className="flex sm:h-full md:h-screen flex-col items-center justify-center">
       <div>
+      <CurrentSongProvider>
         <MusicPlayer />
+      </CurrentSongProvider>
       </div>
       <div>
         <Footer />
---
+++ src/components/CurrentSongContext.tsx
@@ -0,0 +1,32 @@
+import { createContext, useContext, useState, ReactNode } from 'react';
+
+interface Track {
+  id: string | number;
+  title: string;
+  artist: string;
+  duration: number;
+}
+
+const CurrentSongContext = createContext<{
+  currentSong: Track | null;
+  setCurrentSong: (track: Track) => void;
+} | undefined>(undefined);
+
+
+export const CurrentSongProvider = ({ children }: { children: ReactNode }) => {
+  const [currentSong, setCurrentSong] = useState<Track | null>(null);
+
+  return (
+    <CurrentSongContext.Provider value={{ currentSong, setCurrentSong }}>
+      {children}
+    </CurrentSongContext.Provider>
+  );
+};
+
+export const useCurrentSong = () => {
+  const context = useContext(CurrentSongContext);
+  if (!context) {
+    throw new Error('useCurrentSong must be used within a CurrentSongProvider');
+  }
+  return context;
+};
