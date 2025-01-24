b5466b3ce7d814b615b63d0b06016fc19af524a5
chdrchz <saavannaa.rose@gmail.com>
2025-01-24 11:28:54 -0600

    removing useless divs

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,13 +3,9 @@ import Footer from "./Footer.tsx";
 
 function App() {
   return (
-    <div className="flex sm:h-full md:h-screen flex-col items-center justify-center">
-      <div>
-        <MusicPlayer />
-      </div>
-      <div>
-        <Footer />
-      </div>
+    <div className="flex flex-col items-center justify-center sm:h-full md:h-screen">
+      <MusicPlayer />
+      <Footer />
     </div>
   );
 }
